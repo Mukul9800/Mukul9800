077840cd43424fbf905ceb69f9da6e73822b0abe
Mukul Kumar <113937317+Mukul9800@users.noreply.github.com>
2023-07-22 11:41:51 +0530

    Add files via upload

---
+++ game2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int SnakeWaterGun(char you, char comp)
+{
+//Draw condition
+    if (you == comp)
+    {
+        return 0;
+    }
+//Non-Draw condition
+    if (you == 's' && comp == 'g')
+    {
+        return -1;
+    }
+    else if (you == 'g' && comp == 's')
+    {
+        return 1;
+    }
+    if (you == 's' && comp == 'w')
+    {
+        return 1;
+    }
+    else if (you == 'w' && comp == 's')
+    {
+        return -1;
+    }
+    if (you == 'g' && comp == 'w')
+    {
+        return -1;
+    }
+    else if (you == 'w' && comp == 'g')
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    char you, comp;
+
+    srand(time(0));
+    int number = rand() % 100 + 1;
+
+    if (number < 33)
+    {
+        comp = 's';
+    }
+    else if (number > 33 && number < 66)
+    {
+        comp = 'w';
+    }
+    else
+    {
+        comp = 'g';
+    }
+
+    printf("Enter 's' for snake, 'w' for water and 'g' for gun\n");
+    scanf("%c", &you);
+    int result = SnakeWaterGun(you, comp);
+
+    if (result == 0)
+    {
+        printf("Game Draw!\n");
+    }
+    else if (result == 1)
+    {
+        printf("You Win!\n");
+    }
+    else
+    {
+        printf("You Lose!\n");
+    }
+    printf("You chose %c and computer chose %c. ", you, comp);
+    return 0;
+}
